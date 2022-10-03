b6012f4bdf4321e0148f5665b05ac805d95d803b
Kai McGregor <kmcgreg@ibm.com>
2022-10-03 14:29:02 -0400

    Added centos script file

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,26 @@
+#!/bin.bash
+
+yum update -y --exclude=kernal
+yum install -y nano git unzip screen
+
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/wwww/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+yum install -y php php-cli php-common php-devel php-mysql
+
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+service httpd restart
